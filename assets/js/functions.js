43cebbca79daa62ff28325a3b7dc7fa458427881
Eddius Maximus <edwardobryan@gmail.com>
2023-05-19 15:30:32 -0500

    sldkjf

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,18 +1,47 @@
-// Browser detection for when you get desperate. A measure of last resort.
-
-// http://rog.ie/post/9089341529/html5boilerplatejs
-// sample CSS: html[data-useragent*='Chrome/13.0'] { ... }
-
-// Uncomment the below to use:
-// var b = document.documentElement;
-// b.setAttribute('data-useragent',  navigator.userAgent);
-// b.setAttribute('data-platform', navigator.platform);
-
-
 function initPage(){
 
-	// your functions go here
+	const gallery = document.querySelector(".gallery");
+	const arrowLeft = document.querySelector(".arrow-left");
+	const arrowRight = document.querySelector(".arrow-right");
+	const galleryContainer = document.getElementById('gallery-container');
+	const showGalleryButton = document.getElementById('show-gallery');
 
-	console.log('page loaded');
+	let translateX = 0;
+	const imageWidth = 250;
+	const numImages = gallery.children.length;
+
+	arrowLeft.addEventListener("click", () => {
+	  if (translateX < 0) {
+		translateX += imageWidth + 20;
+		gallery.style.transform = `translateX(${translateX}px)`;
+	  }
+	});
+
+	arrowRight.addEventListener("click", () => {
+	  if (translateX > -(numImages - 4) * (imageWidth + 20)) {
+		translateX -= imageWidth + 20;
+		gallery.style.transform = `translateX(${translateX}px)`;
+	  }
+	});
+
+	const overlay = document.querySelector(".overlay");
+	const overlayImage = document.createElement("img");
+	overlayImage.classList.add("overlay-image");
+	overlay.appendChild(overlayImage);
+
+	gallery.addEventListener("click", (event) => {
+	  if (event.target.tagName === "IMG") {
+		overlayImage.src = event.target.src;
+		overlay.classList.add("active");
+	  }
+	});
+
+	overlay.addEventListener("click", () => {
+	  overlay.classList.remove("active");
+	});
+	
+	$( "#show-gallery" ).on( "click", function() {
+	  $( '#gallery-container' ).toggleClass( "show" );
+	});
 
 };
